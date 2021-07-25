0548d1b6d7e611fb3b1fd8c01a59ad9bc701b2a8
RadishBEAR <44549468+RadishBEAR@users.noreply.github.com>
2021-07-25 12:45:11 +0000

    Deploying to idc from @ RadishBEAR/ori-ui@a00a248a73220d78207f74be22714943e20823ec 🚀

--- packages/test/index.ts
+++
@@ -1,8 +0,0 @@
-import { VueConstructor } from 'vue';
-import OriTest from './src/test.vue';
-
-(OriTest).install = (Vue: VueConstructor) => {
-	Vue.component(OriTest.entryName, OriTest);
-};
-
-export default OriTest;
--- typings/vue-prototype.d.ts
+++
@@ -1,8 +0,0 @@
-declare module '*.vue' {
-  import { ComponentOptions,VueConstructor } from 'vue'
-  const component: ComponentOptions<Vue> & {
-    install(app: VueConstructor): void,
-    entryName: string;
-  }
-  export default component
-}
